624005252e28f7b30c57619b62b9a245dcd544ef
Shouzhe Zhang <45340692+sxzhang1993@users.noreply.github.com>
2022-06-06 12:42:38 -0500

    Add files via upload

---
+++ cdp-print.cu
@@ -0,0 +1,107 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+// Variable on the GPU used to generate unique id of blocks.
+//__device__ only use and executed on device
+
+__device__ int g_uids = 0;
+
+// Print a simple message to signal the block which is currently executing.
+__device__ void print_info(int depth, int thread, int uid, int parent_uid) {
+  //threadidx id for each thread in a block
+  if (threadIdx.x == 0) {
+    if (depth == 0)
+      printf("BLOCK %d launched by the host\n", uid);
+    else {
+      char buffer[32];
+
+      for (int i = 0; i < depth; ++i) {
+        buffer[3 * i + 0] = '|';
+        buffer[3 * i + 1] = ' ';
+        buffer[3 * i + 2] = ' ';
+      }
+
+      buffer[3 * depth] = '\0';
+      printf("%sBLOCK %d launched by thread %d of block %d\n", buffer, uid,
+             thread, parent_uid);
+    }
+  }
+
+  __syncthreads();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// The kernel using CUDA dynamic parallelism.
+//
+// It generates a unique identifier for each block. Prints the information
+// about that block. Finally, if the 'max_depth' has not been reached, the
+// block launches new blocks directly from the GPU.
+////////////////////////////////////////////////////////////////////////////////
+__global__ void cdp_kernel(int max_depth, int depth, int thread,
+                           int parent_uid) {
+  // We create a unique ID per block. Thread 0 does that and shares the value
+  // with the other threads.
+  __shared__ int s_uid;
+
+  if (threadIdx.x == 0) {
+    s_uid = atomicAdd(&g_uids, 1);
+  }
+
+  __syncthreads();
+
+  // We print the ID of the block and information about its parent.
+  print_info(depth, thread, s_uid, parent_uid);
+
+  // We launch new blocks if we haven't reached the max_depth yet.
+  if (++depth >= max_depth) {
+    return;
+  }
+
+  cdp_kernel<<<gridDim.x, blockDim.x>>>(max_depth, depth, threadIdx.x, s_uid);
+}
+
+int main(int argc, char **argv) {
+  printf("starting Simple Print (CUDA Dynamic Parallelism)\n");
+
+  int max_depth = 2;
+
+
+
+  // Print a message describing what the sample does.
+  printf(
+      "*********************************************************************"
+      "******\n");
+  printf(
+      "The CPU launches 2 blocks of 2 threads each. On the device each thread "
+      "will\n");
+  printf(
+      "launch 2 blocks of 2 threads each. The GPU we will do that "
+      "recursively\n");
+  printf("until it reaches max_depth=%d\n\n", max_depth);
+  printf("In total 2");
+  int num_blocks = 2, sum = 2;
+
+  for (int i = 1; i < max_depth; ++i) {
+    num_blocks *= 4;
+    printf("+%d", num_blocks);
+    sum += num_blocks;
+  }
+
+  printf("=%d blocks are launched!!! (%d from the GPU)\n", sum, sum - 2);
+  printf(
+      "************************************************************************"
+      "***\n\n");
+
+  // We set the recursion limit for CDP to max_depth.
+  cudaDeviceSetLimit(cudaLimitDevRuntimeSyncDepth, max_depth);
+
+  // Launch the kernel from the CPU.
+  printf("Launching cdp_kernel() with CUDA Dynamic Parallelism:\n\n");
+  cdp_kernel<<<2, 2>>>(max_depth, 0, 0, -1);
+
+  // Finalize.
+  cudaDeviceSynchronize();
+
+  exit(EXIT_SUCCESS);
+}
